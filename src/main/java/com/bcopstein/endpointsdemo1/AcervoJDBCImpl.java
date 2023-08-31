c9cd39de01d43024e8373fdd20bcfc2a09ed73ed
Március Moraes Vargas <marcius.vargas@acad.pucrs.br>
2023-08-31 01:18:19 -0300

    scripts added

---
+++ src/main/java/com/bcopstein/endpointsdemo1/AcervoJDBCImpl.java
@@ -0,0 +1,5 @@
+package com.bcopstein.endpointsdemo1;
+
+public class AcervoJDBCImpl implements IAcervoRepository {
+    
+}
---
+++ src/main/java/com/bcopstein/endpointsdemo1/AcervoMemorialImpl.java
@@ -0,0 +1,58 @@
+package com.bcopstein.endpointsdemo1;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AcervoMemorialImpl implements IAcervoRepository {
+    private List<Livro> livros;
+
+    public AcervoMemorialImpl(){
+        livros = new LinkedList<>();
+
+        livros.add(new Livro(10,"Introdução ao Java","Huguinho Pato",2022));
+        livros.add(new Livro(20,"Introdução ao Spring-Boot","Zezinho Pato",2020));
+        livros.add(new Livro(15,"Principios SOLID","Luizinho Pato",2023));
+        livros.add(new Livro(17,"Padroes de Projeto","Lala Pato",2019));
+    }
+
+    @Override
+    public List<Livro>getAll(){  return livros; }
+
+    @Override
+    public boolean cadastraLivroNovo(Livro livro) {
+        livros.add(livro);
+        return true;
+    }
+
+    @Override
+    public boolean removeLivro(int codigo) {
+        List<Livro> tmp = livros.stream()
+            .filter(livro->livro.codigo() == codigo)
+            .toList();
+        return tmp.removeAll(tmp);
+    }
+
+    @Override
+    public Livro getPorId(int id){
+        return livros.get(id);
+    };
+
+    @Override
+    public List<Livro> getAutor(String autor){
+        return livros;
+    };
+
+
+    public List<Livro> getTitulo(String titulo){
+        return livros;
+    }
+
+    public List<Livro> getAno(int ano){
+        return livros;
+    }
+}
+    
+    
